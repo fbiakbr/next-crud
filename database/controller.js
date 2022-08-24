af4692a3e82f29b10b269be20a30e77d52036a52
fbiakbr <febiakbar19@gmail.com>
2022-08-24 09:27:43 +0700

    Post data

--- database/controller.js
+++ database/controller.js
@@ -8,3 +8,17 @@ export async function getUsers(req, res){
         res.status(404).json({ error: "Error while fetching data" })
     }
 }
+
+export async function postUser(req, res){
+    try {
+        const formData = req.body
+        if (!formData) {
+            return res.status(404).json({ error: "Form data not provided" })
+        }
+        Users.create(formData, function(err, data){
+            return res.status(200).json(data)
+        })
+    } catch (error) {
+        return res.status(404).json({ error: "Error while fetching data" })
+    }
+}
